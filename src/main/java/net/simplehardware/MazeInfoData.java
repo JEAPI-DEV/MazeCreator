357c999a4796b55fdc464ae5849b5f76e1dd034c
JEAPI_DEV <abstractdev7@gmail.com>
2025-10-20 21:44:27 +0200

    update data structure for forms support

--- src/main/java/net/simplehardware/MazeInfoData.java
+++ src/main/java/net/simplehardware/MazeInfoData.java
@@ -3,8 +3,24 @@ package net.simplehardware;
 import java.util.List;
 
 public class MazeInfoData {
+
     public String id;
     public String name;
-    public List<Object> forms;
+    public List<FormInfo> forms;
     public String maze;
+    public int maxPlayers = 4;
+    public int recommendedLevel = 2;
+}
+
+class FormInfo {
+
+    public char id;
+    public String name;
+
+    public FormInfo() {}
+
+    public FormInfo(char id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
