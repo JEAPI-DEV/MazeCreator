12df11af1165e7bd6f2ada19e267424b934ff403
JEAPI_DEV <abstractdev7@gmail.com>
2025-10-20 21:43:05 +0200

    integrate forms toolbar in main editor window

--- src/main/java/net/simplehardware/MazeEditor.java
+++ src/main/java/net/simplehardware/MazeEditor.java
@@ -1,9 +1,10 @@
 package net.simplehardware;
 
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class MazeEditor extends JFrame {
+
     private Mode currentMode = Mode.FLOOR;
     private int currentPlayerId = 1;
 
@@ -20,15 +21,27 @@ public class MazeEditor extends JFrame {
         ToolbarFactory toolbarFactory = new ToolbarFactory(this, mazeGrid);
         add(toolbarFactory.createTopToolbar(), BorderLayout.NORTH);
         add(toolbarFactory.createLeftToolbar(), BorderLayout.WEST);
+        add(toolbarFactory.createFormsToolbar(), BorderLayout.SOUTH);
 
         setVisible(true);
     }
 
     // Mode and player getters/setters used by CellButton and toolbars
-    public void setCurrentMode(Mode mode) { this.currentMode = mode; }
-    public Mode getCurrentMode() { return currentMode; }
-    public void setCurrentPlayerId(int id) { this.currentPlayerId = id; }
-    public int getCurrentPlayerId() { return currentPlayerId; }
+    public void setCurrentMode(Mode mode) {
+        this.currentMode = mode;
+    }
+
+    public Mode getCurrentMode() {
+        return currentMode;
+    }
+
+    public void setCurrentPlayerId(int id) {
+        this.currentPlayerId = id;
+    }
+
+    public int getCurrentPlayerId() {
+        return currentPlayerId;
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(MazeEditor::new);
